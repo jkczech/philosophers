d3a391543f65a8a4b9c902272fc44ea26b28e80d
Josef Koupý <josef.koupy@seznam.cz>
2024-01-19 13:29:43 +0100

    added allowed libraries

---
+++ philo/include/philosophers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 13:19:04 by jkoupy            #+#    #+#             */
+/*   Updated: 2024/01/19 13:28:35 by jkoupy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//memset
+#include <string.h>
+//printf
+#include <stdio.h>
+//malloc, free,
+#include <stdlib.h>
+//write, usleep
+#include <unistd.h>
+//gettimeofday
+#include <sys/time.h>
+// pthread_detach, pthread_join, pthread_mutex_init, pthread_mutex_destroy
+// pthread_mutex_lock, pthread_mutex_unlock
+#include <pthread.h>
